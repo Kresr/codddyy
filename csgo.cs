80791928154c00dc56706f4f9d1442e81608857a
Kresr <112491181+Kresr@users.noreply.github.com>
2022-10-14 10:34:14 +0300

    Add files via upload

--- csgo.cs
+++ csgo.cs
@@ -1,6 +1,6 @@
 using System;
 
-// 2022-10-01 09:01:32.225095200 UTC
+// 2022-10-14 07:33:17.567750100 UTC
 
 namespace hazedumper
 {
@@ -148,7 +148,7 @@ namespace hazedumper
         public const Int32 dwZoomSensitivityRatioPtr = 0xDCD620;
         public const Int32 dwbSendPackets = 0xD8632;
         public const Int32 dwppDirect3DDevice9 = 0xA6050;
-        public const Int32 find_hud_element = 0x26BA5D20;
+        public const Int32 find_hud_element = 0x26865D20;
         public const Int32 force_update_spectator_glow = 0x3BE5FA;
         public const Int32 interface_engine_cvar = 0x3EA3C;
         public const Int32 is_c4_owner = 0x3CB670;
--- csgo.hpp
+++ csgo.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstddef>
 
-// 2022-10-01 09:01:32.225095200 UTC
+// 2022-10-14 07:33:17.567750100 UTC
 
 namespace hazedumper {
 namespace netvars {
@@ -146,7 +146,7 @@ constexpr ::std::ptrdiff_t dwYawPtr = 0xDC6E38;
 constexpr ::std::ptrdiff_t dwZoomSensitivityRatioPtr = 0xDCD620;
 constexpr ::std::ptrdiff_t dwbSendPackets = 0xD8632;
 constexpr ::std::ptrdiff_t dwppDirect3DDevice9 = 0xA6050;
-constexpr ::std::ptrdiff_t find_hud_element = 0x26BA5D20;
+constexpr ::std::ptrdiff_t find_hud_element = 0x26865D20;
 constexpr ::std::ptrdiff_t force_update_spectator_glow = 0x3BE5FA;
 constexpr ::std::ptrdiff_t interface_engine_cvar = 0x3EA3C;
 constexpr ::std::ptrdiff_t is_c4_owner = 0x3CB670;
